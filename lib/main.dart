4681ebee527934c412313a55727ed19b2fe14d29
Harold <Edselcabaluna21@gmail.com>
2025-04-19 22:47:06 +0800

    firebase configured, proceeding to supabase storage and setting up firebase auth

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,15 @@ import 'package:fitsync/loginpage/login.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   await Supabase.initialize(
     url: 'https://jkirfownrjtaqydbxcgx.supabase.co',
