0b66a1c47db63b95c1b0b2b194b2a02f4f8c2df0
Harold <Edselcabaluna21@gmail.com>
2025-04-27 10:53:39 +0800

    for development saving files before changes

--- lib/homepage/home.dart
+++ lib/homepage/home.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:fitsync/mainscreen/boxes.dart';
+import 'package:fitsync/MainScreen/boxes.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class Home extends StatefulWidget {
